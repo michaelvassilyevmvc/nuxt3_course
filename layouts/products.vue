9b129fa6cdc3d08b935f61648e42566f8b36aeae
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2022-12-09 02:32:44 +0600

    Create layouts for default and products pages

---
+++ layouts/products.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <NuxtLink to="/products">Nuxt Dojo Merch</NuxtLink>
+      </nav>
+    </header>
+    <div>
+      <slot />
+    </div>
+    <footer>
+      <ul>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+      </ul>
+      <ul>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+      </ul>
+      <ul>
+        <li><NuxtLink to="/products">Merch</NuxtLink></li>
+      </ul>
+    </footer>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.router-link-exact-active {
+  color: #12b488;
+}
+</style>
