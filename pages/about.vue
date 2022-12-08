8380df700bfcc25a6214b3009c990be1c9ae2262
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2022-12-09 01:36:21 +0600

    Create home, about and product/index pages

---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi architecto
+      ratione quia consequatur reiciendis similique, facilis quae unde?
+      Assumenda, laboriosam?
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Id minus rem
+      dolor. Ex molestiae esse tenetur eveniet ratione temporibus illum!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam,
+      laboriosam itaque tempore aperiam exercitationem asperiores.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
