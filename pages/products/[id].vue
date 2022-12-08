3385f724a9fda0e2fae2a470691d5b61facd9196
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2022-12-09 01:48:33 +0600

    Create detail page with [id] params from route

---
+++ pages/products/[id].vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Product detail for {{ id }}</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione aut quod
+      voluptatem aliquid rerum? Placeat commodi adipisci fugit voluptatem
+      aspernatur.
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+</script>
+
+<style scoped></style>
