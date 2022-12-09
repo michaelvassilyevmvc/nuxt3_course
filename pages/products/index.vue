815e54b391146dfb7a7b5df7a7c361f6ddf065b5
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2022-12-09 09:42:48 +0600

    Create fetch data for products page

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
-    <h2>Products</h2>
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="p in products">
+        <NuxtLink :to="`/products/${p.id}`">{{ p.title }}</NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
+const { data: products } = await useFetch("https://fakestoreapi.com/products");
+
 definePageMeta({
   layout: "products",
 });
